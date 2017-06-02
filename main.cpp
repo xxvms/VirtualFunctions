cc501501b9cc48a68a3125104b37af2e78bede83
xxvms <tom@pcservicegroup.co.uk>
2017-06-02 12:07:06 +0100

    Book Programming principles in C++ Chapter 11 page 506 accessig derived classes without virtaul function

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+/*
+ * Virtual Functions -
+ * this is the example where we have functions with same names and we will access this with pointers
+ */
+class Base                                          //base class
+{
+public:
+	void show()                                     // normal function
+	{
+		std::cout << "Base\n" << std::endl;
+	}
+};
+class Derived1: public Base                         // derived class 1
+{
+public:
+	void show()
+	{
+		std::cout << "Derived 1\n" << std::endl;
+	}
+};
+class Derived2: public Base                        // derived class 2
+{
+public:
+	void show()
+	{
+		std::cout << "Derived 2\n" << std::endl;
+	}
+};
+
+int main()
+{
+	Derived1 derived1;                              // object of derived class 1
+	Derived2 derived2;                              // object of derived class 2
+	Base* ptrBase;                                  // pointer to base class
+	
+	ptrBase = &derived1;                            // put address of derived1 in the pointer
+	ptrBase->show();                                // execute show()
+	
+	ptrBase = &derived2;                            // put address of derived2 in the pointer
+	ptrBase->show();                                // execute show()
+	
+	system("pause");
+	return 0;
+}
